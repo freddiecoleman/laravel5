ecd3c5321f5b3787e852365da0e7a9eff081ba3f
Freddie Coleman <f.coleman@hotmail.co.uk>
2014-10-03 19:50:28 +0100

    create request

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -1,14 +1,15 @@
 @extends('layout')
 
 @section('content')
-    {!! Form::open() !!}
+    <h1>Create a new task</h1>
+    {!! Form::open(['route' => 'tasks.store']) !!}
         <div class="form-group">
             {!! Form::label('name', 'Name:') !!}
             {!! Form::text('name', null, ['class' => 'form-control']) !!}
         </div>
         <div class="form-group">
             {!! Form::label('comment', 'Comment:') !!}
-            {!! Form::textarea('comment', null, ['class' => 'form-control']) !!}
+            {!! Form::textarea('comment', null, ['class' => 'form-control', 'required']) !!}
         </div>
         <div class="form-group">
             {!! Form::submit('Submit', ['class' => 'btn tn-primary form-control']) !!}
